14030c3700aac2671d8fc924fac7ab26e0977e1c
gabrieltenachous <gabriel_oliveira_f_sousa@outlook.com>
2020-08-09 10:48:46 -0300

    PrimeiroCRUD

---
+++ PrimeiroCRUD/Migrations/20200809132833_PrimeiroMigrationAtualizada.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PrimeiroCRUD.Migrations
+{
+    public partial class PrimeiroMigrationAtualizada : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
